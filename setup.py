8a7dba2e663b24f07d99b11bfcdf3314ab304cdd
Hiroshi Perera <hiroshiperera007@gmail.com>
2024-05-27 12:36:34 +0530

    This is the commit for web application

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("REASME.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-MLflow-DVC"
+AUTHOR_USER_NAME = "hirplk"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "hiroshiperera007@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version = __version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "A small python package for CNN app",
+    Long_description = long_description,
+    Long_description_content = "text/markdown",
+    url = f"https://giithub.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+
+)
